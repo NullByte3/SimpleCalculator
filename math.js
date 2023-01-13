40236de426e03e8361f829dacfa72c2ed64c1c04
NullByte3 <122549857+NullByte3@users.noreply.github.com>
2023-01-13 11:20:12 +0200

    Implemented new math stuff
    I basically added more functions and wrote a bit better code.

--- math.js
+++ math.js
@@ -1,8 +1,48 @@
-function math () {
+let lastButton = plus
+
+function plus () {
+  lastButton = plus
   var num1 = document.getElementById('n1').value
   var num2 = document.getElementById('n2').value
   var sum = parseFloat(num1) + parseFloat(num2)
   var resultLabel = document.getElementById('result')
   resultLabel.innerHTML =
     'Summa on <strong>' + sum.toLocaleString('en-US') + '</strong>'
+}
+
+function minus () {
+  lastButton = minus
+  var num1 = document.getElementById('n1').value
+  var num2 = document.getElementById('n2').value
+  var sum = parseFloat(num1) - parseFloat(num2)
+  var resultLabel = document.getElementById('result')
+  resultLabel.innerHTML =
+    'Summa on <strong>' + sum.toLocaleString('en-US') + '</strong>'
+}
+
+function divide () {
+  lastButton = divide
+  var num1 = document.getElementById('n1').value
+  var num2 = document.getElementById('n2').value
+  var sum = parseFloat(num1) / parseFloat(num2)
+  var resultLabel = document.getElementById('result')
+  resultLabel.innerHTML =
+    'Summa on <strong>' + sum.toLocaleString('en-US') + '</strong>'
+}
+
+function multiply () {
+  lastButton = multiply
+  var num1 = document.getElementById('n1').value
+  var num2 = document.getElementById('n2').value
+  var sum = parseFloat(num1) * parseFloat(num2)
+  var resultLabel = document.getElementById('result')
+  resultLabel.innerHTML = 'Summa on ' + sum
+}
+function clear () {
+  var resultLabel = document.getElementById('result')
+  resultLabel.innerHTML = 'Summa on ...'
+}
+
+function last () {
+  if (lastButton != null) lastButton()
 }
